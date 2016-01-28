162638fe31b4a648a14e8d94457e6b3d09bf5635
literacyapp <info@literacyapp.org>
2016-01-28 16:22:15 +0100

    Update BootReciever.java

--- app/src/main/java/org/literacyapp/BootReciever.java
+++ app/src/main/java/org/literacyapp/BootReciever.java
@@ -5,13 +5,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class BootReciever extends BroadcastReceiver{
+public class BootReciever extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent bootintent = new Intent(context, MainActivity.class);
         bootintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(bootintent);
-
     }
 }
