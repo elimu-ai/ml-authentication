15983b0f111e243250b89428080f5c219f3ae864
adrygll2 <nb17adri@gmail.com>
2016-02-01 23:00:09 +0100

    Code edit following the Java code style conventions

--- app/src/main/java/org/literacyapp/MainActivity.java
+++ app/src/main/java/org/literacyapp/MainActivity.java
@@ -3,7 +3,7 @@ package org.literacyapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
