a351ba255871b5fcd24b33ea0def2c3bffae3743
Jo Grimstad <jo@literacyapp.org>
2016-08-11 12:30:52 +0300

    TagItemsActivity

--- app/src/main/java/org/literacyapp/task/TagItemsActivity.java
+++ app/src/main/java/org/literacyapp/task/TagItemsActivity.java
@@ -1,20 +1,25 @@
 package org.literacyapp.task;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import org.literacyapp.R;
 
 public class TagItemsActivity extends AppCompatActivity {
 
+    private ImageView mTagItemsImageView;
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+    protected void onCreate(Bundle savedInstanceState) {
         Log.i(getClass().getName(), "onCreate");
-        super.onCreate(savedInstanceState, persistentState);
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_tag_items);
+
+        mTagItemsImageView = (ImageView) findViewById(R.id.tagItemsImageView);
     }
 
     @Override
@@ -22,6 +27,21 @@ public class TagItemsActivity extends AppCompatActivity {
         Log.i(getClass().getName(), "onStart");
         super.onStart();
 
-        // TODO
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        mTagItemsImageView.animate()
+                .setStartDelay(500)
+                .rotation(360)
+                .scaleX(2)
+                .scaleY(2)
+                .setDuration(1000)
+                .start();
     }
 }
