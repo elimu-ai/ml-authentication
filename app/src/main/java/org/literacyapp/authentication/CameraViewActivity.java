49d4bf207e81d2e05c84dba6b3296d8324e3ce54
Mike <schaemik@students.zhaw.ch>
2016-09-25 14:33:08 +0200

    Added Camera View Activity

---
+++ app/src/main/java/org/literacyapp/authentication/CameraViewActivity.java
@@ -0,0 +1,15 @@
+package org.literacyapp.authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.literacyapp.R;
+
+public class CameraViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_view);
+    }
+}
