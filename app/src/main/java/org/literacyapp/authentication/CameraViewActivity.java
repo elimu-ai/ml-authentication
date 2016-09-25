d3215609706bcca9a4186aa95cb468267366097a
Mike <schaemik@students.zhaw.ch>
2016-09-25 17:05:31 +0200

    Added Start with CameraActivityView for testing

--- app/src/main/java/org/literacyapp/authentication/CameraViewActivity.java
+++ app/src/main/java/org/literacyapp/authentication/CameraViewActivity.java
@@ -4,12 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.literacyapp.R;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
 
-public class CameraViewActivity extends AppCompatActivity {
+public class CameraViewActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_view);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
     }
 }
