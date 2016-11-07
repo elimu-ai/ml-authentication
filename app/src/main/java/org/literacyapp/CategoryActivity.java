dc523910473cfa81efa32525d8c3d47b93e1aa8b
Adrian Schäfer <contact@adrianschaefer.com>
2016-11-07 17:10:24 +0100

    remove fullscreen

--- app/src/main/java/org/literacyapp/CategoryActivity.java
+++ app/src/main/java/org/literacyapp/CategoryActivity.java
@@ -170,14 +170,14 @@ public class CategoryActivity extends AppCompatActivity {
         Log.d(getClass(), "onStart");
         super.onStart();
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(
+//                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     void updateIndicators(int position) {
--- app/src/main/java/org/literacyapp/task/GraphemeActivity.java
+++ app/src/main/java/org/literacyapp/task/GraphemeActivity.java
@@ -54,14 +54,14 @@ public class GraphemeActivity extends AppCompatActivity {
         Log.d(getClass(), "onStart");
         super.onStart();
 
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         Letter letter = letterDao.queryBuilder()
                 .where(LetterDao.Properties.Text.eq("a")) // TODO: fetch value dynamically
