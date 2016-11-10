40b47b16a640a5a0d051fa7fa96573c1270cb40c
sladomic <sladomic@students.zhaw.ch>
2016-11-10 23:15:27 +0100

    Remove IntentService import

--- app/src/main/java/org/literacyapp/authentication/CameraViewActivity.java
+++ app/src/main/java/org/literacyapp/authentication/CameraViewActivity.java
@@ -1,6 +1,5 @@
 package org.literacyapp.authentication;
 
-import android.app.IntentService;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
