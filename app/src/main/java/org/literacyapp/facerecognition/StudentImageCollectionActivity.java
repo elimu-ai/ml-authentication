c67af2862950eaeb6d024d23a62e18993b60f019
Jo Grimstad <jo@literacyapp.org>
2016-11-19 15:52:38 +0300

    #186 Create activity for dataset collection

--- app/src/main/java/org/literacyapp/facerecognition/StudentImageCollectionActivity.java
+++ app/src/main/java/org/literacyapp/facerecognition/StudentImageCollectionActivity.java
@@ -2,12 +2,18 @@ package org.literacyapp.facerecognition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.literacyapp.R;
 
 public class StudentImageCollectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getClass().getName(), "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_image_collection);
     }
+
+
 }
