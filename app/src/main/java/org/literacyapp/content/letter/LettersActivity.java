02f7598e07fee0703969a6f362b641b53c18bc5c
Jo Grimstad <jo@literacyapp.org>
2016-11-19 11:51:03 +0300

    #183 List letters

--- app/src/main/java/org/literacyapp/content/letter/LettersActivity.java
+++ app/src/main/java/org/literacyapp/content/letter/LettersActivity.java
@@ -2,11 +2,11 @@ package org.literacyapp.content.letter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.GridLayout;
 import android.widget.TextView;
 
 import org.literacyapp.LiteracyApplication;
