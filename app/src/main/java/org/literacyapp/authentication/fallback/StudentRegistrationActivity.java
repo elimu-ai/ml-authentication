6a33bc7b09f97f158ae8487c1d7fbe732fa4e33d
Jo Grimstad <jo@literacyapp.org>
2016-12-10 19:07:36 +0300

    #250 Add fall-back authentication

--- app/src/main/java/org/literacyapp/authentication/fallback/StudentRegistrationActivity.java
+++ app/src/main/java/org/literacyapp/authentication/fallback/StudentRegistrationActivity.java
@@ -2,12 +2,28 @@ package org.literacyapp.authentication.fallback;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.literacyapp.R;
+import org.literacyapp.util.MediaPlayerHelper;
 
 public class StudentRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getClass().getName(), "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_registration);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(getClass().getName(), "onStart");
+        super.onStart();
+
+        MediaPlayerHelper.play(getApplicationContext(), R.raw.instruction_student_registration);
+
+
     }
 }
