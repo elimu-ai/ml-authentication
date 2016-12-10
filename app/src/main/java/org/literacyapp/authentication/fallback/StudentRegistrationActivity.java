3423e2391b7844f1ddf4c8ed546bb8305ec534f4
Jo Grimstad <jo@literacyapp.org>
2016-12-10 19:19:23 +0300

    #250 Play an animation to indicate that the button should be pressed

--- app/src/main/java/org/literacyapp/authentication/fallback/StudentRegistrationActivity.java
+++ app/src/main/java/org/literacyapp/authentication/fallback/StudentRegistrationActivity.java
@@ -1,13 +1,19 @@
 package org.literacyapp.authentication.fallback;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.literacyapp.R;
 import org.literacyapp.util.MediaPlayerHelper;
 
 public class StudentRegistrationActivity extends AppCompatActivity {
+
+    private Button studentRegistrationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,8 @@ public class StudentRegistrationActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_registration);
+
+        studentRegistrationButton = (Button) findViewById(R.id.studentRegistrationButton);
     }
 
     @Override
@@ -24,6 +32,27 @@ public class StudentRegistrationActivity extends AppCompatActivity {
 
         MediaPlayerHelper.play(getApplicationContext(), R.raw.instruction_student_registration);
 
+        // Play an animation to indicate that the button should be pressed
+        studentRegistrationButton.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final long duration = 300;
 
+                final ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(studentRegistrationButton, View.SCALE_X, 1f, 1.2f, 1f);
+                scaleXAnimator.setDuration(duration);
+                scaleXAnimator.setRepeatCount(1);
+
+                final ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(studentRegistrationButton, View.SCALE_Y, 1f, 1.2f, 1f);
+                scaleYAnimator.setDuration(duration);
+                scaleYAnimator.setRepeatCount(1);
+
+                scaleXAnimator.start();
+                scaleYAnimator.start();
+
+                final AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.play(scaleXAnimator).with(scaleYAnimator);
+                animatorSet.start();
+            }
+        }, 4000);
     }
 }
