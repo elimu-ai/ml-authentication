bec2de7c4b4c1a310c0e8a30064367e5b955bfaa
Jo Grimstad <jo@literacyapp.org>
2016-12-11 13:35:40 +0300

    #250 Take picture of face with default Android camera

--- app/src/main/java/org/literacyapp/authentication/fallback/StudentRegistrationActivity.java
+++ app/src/main/java/org/literacyapp/authentication/fallback/StudentRegistrationActivity.java
@@ -2,18 +2,37 @@ package org.literacyapp.authentication.fallback;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
 
 import org.literacyapp.R;
 import org.literacyapp.util.MediaPlayerHelper;
 
 public class StudentRegistrationActivity extends AppCompatActivity {
 
-    private Button studentRegistrationButton;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private RippleBackground rippleBackground;
+
+    private Button cameraButton;
+
+    private ImageView thumbnailImageView;
+
+    private Bitmap imageBitmap;
+
+    private ImageView checkmarkImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +41,23 @@ public class StudentRegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_registration);
 
-        studentRegistrationButton = (Button) findViewById(R.id.studentRegistrationButton);
+        rippleBackground = (RippleBackground) findViewById(R.id.studentRegistrationRippleBackground);
+        cameraButton = (Button) findViewById(R.id.studentRegistrationButton);
+        thumbnailImageView = (ImageView) findViewById(R.id.studentRegistrationImageView);
+        checkmarkImageView = (ImageView) findViewById(R.id.studentRegistrationCheckmark);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(getClass().getName(), "cameraButton onClick");
+
+                // Take picture with front camera. See https://developer.android.com/training/camera/photobasics.html
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
 
     @Override
@@ -30,29 +65,93 @@ public class StudentRegistrationActivity extends AppCompatActivity {
         Log.i(getClass().getName(), "onStart");
         super.onStart();
 
-        MediaPlayerHelper.play(getApplicationContext(), R.raw.instruction_student_registration);
+        if (imageBitmap == null) {
 
-        // Play an animation to indicate that the button should be pressed
-        studentRegistrationButton.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                final long duration = 300;
+            MediaPlayerHelper.play(getApplicationContext(), R.raw.instruction_student_registration);
 
-                final ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(studentRegistrationButton, View.SCALE_X, 1f, 1.2f, 1f);
-                scaleXAnimator.setDuration(duration);
-                scaleXAnimator.setRepeatCount(1);
+            // Play an animation to indicate that the cameraButton should be pressed
+            cameraButton.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(getClass().getName(), "cameraButton.postDelayed run");
 
-                final ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(studentRegistrationButton, View.SCALE_Y, 1f, 1.2f, 1f);
-                scaleYAnimator.setDuration(duration);
-                scaleYAnimator.setRepeatCount(1);
+                    final long duration = 300;
 
-                scaleXAnimator.start();
-                scaleYAnimator.start();
+                    final ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(cameraButton, View.SCALE_X, 1f, 1.2f, 1f);
+                    scaleXAnimator.setDuration(duration);
 
-                final AnimatorSet animatorSet = new AnimatorSet();
-                animatorSet.play(scaleXAnimator).with(scaleYAnimator);
-                animatorSet.start();
-            }
-        }, 4000);
+                    final ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(cameraButton, View.SCALE_Y, 1f, 1.2f, 1f);
+                    scaleYAnimator.setDuration(duration);
+
+                    scaleXAnimator.start();
+                    scaleYAnimator.start();
+
+                    final AnimatorSet animatorSet = new AnimatorSet();
+                    animatorSet.play(scaleXAnimator).with(scaleYAnimator);
+                    animatorSet.start();
+
+                    rippleBackground.startRippleAnimation();
+                    rippleBackground.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            rippleBackground.stopRippleAnimation();
+                        }
+                    }, 600);
+                }
+            }, 4000);
+        } else {
+            // Replace camera button with image thumbnail
+            cameraButton.setVisibility(View.GONE);
+
+            // Pulsate image thumbnail
+            rippleBackground.startRippleAnimation();
+            rippleBackground.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    rippleBackground.stopRippleAnimation();
+                }
+            }, 600);
+
+            // Animate checkmark
+            checkmarkImageView.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    checkmarkImageView.setVisibility(View.VISIBLE);
+                    Drawable drawable = checkmarkImageView.getDrawable();
+                    ((Animatable) drawable).start();
+
+                    MediaPlayerHelper.play(getApplicationContext(), R.raw.tada);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            MediaPlayerHelper.play(getApplicationContext(), R.raw.instruction_student_registration_good_job);
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    finish();
+                                }
+                            }, 2000);
+                        }
+                    }, 1000);
+                }
+            }, 600);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(getClass().getName(), "onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == REQUEST_IMAGE_CAPTURE) && (resultCode == RESULT_OK)) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            // TODO: detect face(s) in image
+            thumbnailImageView.setImageBitmap(imageBitmap);
+
+            // TODO: store image on SD card
+        }
     }
 }
