31c0d7da278c35a2843045294bdd6abbf209730c
sladomic <michael.sladoje@outlook.com>
2016-12-31 13:48:39 +0100

    Fix #308 AuthenticationActivity crashes if it starts while the screen is turned off and then turned on

--- app/src/main/java/org/literacyapp/service/synchronization/AuthenticationJobService.java
+++ app/src/main/java/org/literacyapp/service/synchronization/AuthenticationJobService.java
@@ -2,8 +2,11 @@ package org.literacyapp.service.synchronization;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.display.DisplayManager;
 import android.util.Log;
+import android.view.Display;
 
 import org.literacyapp.authentication.AuthenticationActivity;
 import org.literacyapp.authentication.TrainingHelper;
@@ -18,11 +21,17 @@ public class AuthenticationJobService extends JobService {
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         Log.i(getClass().getName(), "onStartJob");
-        if (!rescheduleIfTrainingJobServiceIsRunning()){
-            Intent authenticationIntent = new Intent(getApplicationContext(), AuthenticationActivity.class);
-            authenticationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(authenticationIntent);
+        if (!isScreenTurnedOff()){
+            if (!rescheduleIfTrainingJobServiceIsRunning()){
+                Intent authenticationIntent = new Intent(getApplicationContext(), AuthenticationActivity.class);
+                authenticationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(authenticationIntent);
+                Log.i(getClass().getName(), "The Authentication has been started.");
+            }
+        } else {
+            Log.i(getClass().getName(), "The Authentication was skipped because the screen was turned off.");
         }
+
         return false;
     }
 
@@ -40,5 +49,17 @@ public class AuthenticationJobService extends JobService {
             Log.i(getClass().getName(), "The AuthenticationJobService has been rescheduled, because the CPU hungry FaceRecognitionTrainingJobService is running at the moment.");
         }
         return isFaceRecognitionTrainingJobServiceRunning;
+    }
+
+    private boolean isScreenTurnedOff(){
+        boolean isScreenTurnedOff = false;
+        DisplayManager displayManager = (DisplayManager) getApplicationContext().getSystemService(Context.DISPLAY_SERVICE);
+        for (Display display : displayManager.getDisplays()) {
+            if (display.getState() != Display.STATE_ON) {
+                // Skip authentication
+                isScreenTurnedOff = true;
+            }
+        }
+        return isScreenTurnedOff;
     }
 }
